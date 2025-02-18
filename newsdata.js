94ca90dc385da18ff7e3f230f04f52a02f82438f
sungmin <ehdtls54@naver.com>
2025-02-18 09:06:36 +0900

    newsio 시작

---
+++ newsdata.js
@@ -0,0 +1,5 @@
+const api_key = "pub_69486c6c023a3d777623ee4f50e89f6a59251";
+const query = "pizza";
+const url = "https://newsdata.io/api/1/latest";
+const request = `${url}?apikey=${api_key}&q=${query}`;
+console.log(query);
